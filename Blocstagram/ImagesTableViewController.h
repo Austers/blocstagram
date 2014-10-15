213ef31d4976420a2a7e3dc1de598090fbcd41e7
Richie Austerberry <rtausterberry@gmail.com>
2014-10-15 16:01:55 +0800

    Table View controller implemented

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Richie Austerberry on 15/10/2014.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
